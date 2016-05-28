2a0b6c51589636c9402f1b1de93fbe545af45a9a
Mounika <lakshmimounika.ch26@gmail.com>
2016-05-28 12:45:40 +0530

    User authantication functionality

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160525172307) do
+ActiveRecord::Schema.define(version: 20160528063111) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "author",     limit: 255
     t.string   "text",       limit: 255
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
+    t.integer  "user_id",    limit: 4
+    t.integer  "post_id",    limit: 4
   end
 
   create_table "posts", force: :cascade do |t|
@@ -26,16 +28,29 @@ ActiveRecord::Schema.define(version: 20160525172307) do
     t.string   "image",       limit: 255
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
+    t.integer  "user_id",     limit: 4
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "name",       limit: 255
-    t.string   "email",      limit: 255
-    t.string   "gender",     limit: 255
+    t.string   "name",                   limit: 255
+    t.string   "gender",                 limit: 255
     t.date     "dob"
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
-    t.string   "avotor",     limit: 255
+    t.datetime "created_at",                                      null: false
+    t.datetime "updated_at",                                      null: false
+    t.string   "avotor",                 limit: 255
+    t.string   "email",                  limit: 255, default: "", null: false
+    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "reset_password_token",   limit: 255
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
